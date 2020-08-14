ef8bb394f218e6e02e7fa9ead5fcc61b8ca003f3
ParkSiHyeon <S.Hyeon.dev@gmail.com>
2020-08-14 16:09:55 +0900

    chapter03 - 산술 연산자

---
+++ doitjava/src/chapter03/ArithmeticOperator.java
@@ -0,0 +1,27 @@
+package chapter03;
+
+public class ArithmeticOperator {
+
+	public static void main(String[] args) {
+			int intResult;
+			double dRes;
+			
+			intResult = 3+2;
+			System.out.println(intResult);
+			intResult = 3-2;
+			System.out.println(intResult);
+			intResult = 3*2;
+			System.out.println(intResult);
+			intResult = 3/2;
+			System.out.println(intResult);
+			dRes = 3.0/2.0;
+			System.out.println(dRes);
+			intResult = 3%2;
+			System.out.println(intResult);
+			
+			dRes = (double)3/2;
+			System.out.println(dRes);
+		
+	}
+
+}
