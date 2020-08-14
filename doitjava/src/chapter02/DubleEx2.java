dd12594fce05a07cb7f89c59d856f09ef6213a87
ParkSiHyeon <S.Hyeon.dev@gmail.com>
2020-08-14 13:22:00 +0900

    chapter02 - 실수연산의 오류

---
+++ doitjava/src/chapter02/DubleEx2.java
@@ -0,0 +1,11 @@
+package chapter02;
+
+public class DubleEx2 {
+
+	public static void main(String[] args) {
+			double value;
+			value = (1.0e20 + 5) - 1.0e20;
+			System.out.println(value);
+	}
+
+}
