a1ddd99adb597667fedbd9c65c809e0a9d8bf573
ParkSiHyeon <s.hyeon.dev@gmail.com>
2020-09-24 16:21:23 +0900

    chapter13

---
+++ doitjava/src/chapter13/InnerTest.java
@@ -0,0 +1,39 @@
+package chapter13;
+
+class OutClass {
+	private int num = 10;
+	private static int sNum = 20;
+
+	static class InStaticClass {
+		int inNum = 100;
+		static int sInNum = 200;
+
+		
+		void inTest() {
+			System.out.println("InStaticClass inNum = " + inNum + "(내부 클래스의 인스턴스 변수 사용)");
+			System.out.println("InStaticClass sInNum = " + sInNum + "(내부 클래스의 정적 변수 사용)");
+			System.out.println("OutClass sNum = " + sNum + "(외부 클래스의 정적 변수 사용)");
+		}
+
+		static void sTest() {
+			// num += 10;
+			// inNum +=10;
+			System.out.println("OutClass snum = " + sNum + "(외부 클래스의 정적 변수 사용)");
+			System.out.println("InStaticClass sInNum= " + sInNum + "(내부 클래스의 정적 변수 사용)");
+
+		}
+	}
+	// static void sTest() {
+
+}
+
+public class InnerTest {
+	public static void main(String[] args) {
+		OutClass.InStaticClass sInClass = new OutClass.InStaticClass();
+		System.out.println("외부 클래스 이용하여 내부 클래스 기능 호출");
+		sInClass.inTest();
+		System.out.println();
+		System.out.println("정적 내부 클래스의 정적 메서드 호출");
+		OutClass.InStaticClass.sTest();
+	}
+}
