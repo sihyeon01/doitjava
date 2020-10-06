d2344621d336efbf7dba76ba242869e3fb1e7a9c
ParkSiHyeon <s.hyeon.dev@gmail.com>
2020-10-06 15:00:39 +0900

    chapter15 - 바이트단위 스트림

---
+++ doitjava/src/chapter15/FileOutputStreamTest2.java
@@ -0,0 +1,21 @@
+package chapter15;
+
+import java.io.FileOutputStream;
+import java.io.IOException;
+
+public class FileOutputStreamTest2 {
+	public static void main(String[] args) throws IOException {
+		try (FileOutputStream fos = new FileOutputStream("output2.txt,true")) {
+			byte[] bs = new byte[26];
+			byte data = 65; // 'A'의 아스키 값
+			for (int i = 0; i < bs.length; i++) {
+				bs[i] = data;
+				data++;
+			}
+			fos.write(bs); // 배열을 한꺼번에 출력
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		System.out.println("출력이 완료되었습니다");
+	}
+}
