95b6597e621e94b58eb417dbdfc32df6395b9f1d
ParkSiHyeon <s.hyeon.dev@gmail.com>
2020-11-02 16:14:39 +0900

    get, set 연습

---
+++ doitjava/src/Alone/Car.java
@@ -0,0 +1,33 @@
+package Alone;
+
+public class Car {
+	private String company;
+	private String model;
+	private String color;
+	private int maxSpeed;
+	public String getCompany() {
+		return company;
+	}
+	public void setCompany(String company) {
+		this.company = company;
+	}
+	public String getModel() {
+		return model;
+	}
+	public void setModel(String model) {
+		this.model = model;
+	}
+	public String getColor() {
+		return color;
+	}
+	public void setColor(String color) {
+		this.color = color;
+	}
+	public int getMaxSpeed() {
+		return maxSpeed;
+	}
+	public void setMaxSpeed(int maxSpeed) {
+		this.maxSpeed = maxSpeed;
+	}
+	
+}
